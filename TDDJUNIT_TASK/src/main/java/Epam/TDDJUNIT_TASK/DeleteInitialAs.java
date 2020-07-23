e5aa6e0bca1495b05a0450c51577e10346e216e5
Krishna <Krishna@LAPTOP-VIMT5KUU>
2020-07-23 21:08:46 +0530

    "TDD JUNIT TASK VERSION -1"

---
+++ TDDJUNIT_TASK/src/main/java/Epam/TDDJUNIT_TASK/DeleteInitialAs.java
@@ -0,0 +1,31 @@
+package Epam.TDDJUNIT_TASK;
+
+public class DeleteInitialAs {
+	public String removingAifPresentInFirstTwoCharacters(String input) {
+		String output="";
+		if(input.length()>1) {
+			if(input.charAt(0)=='A' && input.charAt(1)=='A') {
+				output=input.substring(2);
+			}
+			else if(input.charAt(0)=='A' && input.charAt(1)!='A') {
+				output= input.substring(1,input.length());
+			}
+			else if(input.charAt(0)!='A' && input.charAt(1)=='A' ) {
+				output= input.substring(0,1) + input.substring(2,input.length());
+			}
+			else {
+				output = input;
+			}
+			
+		}
+		else {
+			if( (input.length()== 1 && input.charAt(0)=='A') || (input.length()==0) ) {
+				output= "";
+			}
+			else {
+				output=input;
+			}
+		}
+		return output;
+	}
+}
